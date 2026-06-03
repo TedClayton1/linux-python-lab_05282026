2b30edee0c084fa87dc475b03c66eda022757b51
Ted <ted_clayton@hotmail.com>
2026-06-03 01:11:47 -0400

    added conditional statement to my bash script.

--- run_audit.sh
+++ run_audit.sh
@@ -9,5 +9,12 @@ mkdir -p reports
 
 python scripts/check_usage.py > reports/disk_report.txt
 
+if [ -f data/server_usage.txt ]
+then
+    echo "Data file found."
+else
+    echo "ERROR: Data file not found."
+fi
+
 echo "Audit complete."
 echo "Report saved to reports/disk_report.txt"
